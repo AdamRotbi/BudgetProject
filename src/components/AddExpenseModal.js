5b02ec8f150a803438d7860df9569a48fd4e54f6
AdamRotbi <adam.rotbi11@gmail.com>
2022-10-30 12:55:28 +0100

    First commit

---
+++ src/components/AddExpenseModal.js
@@ -0,0 +1,66 @@
+import { Form, Modal, Button } from "react-bootstrap"
+import { useRef } from "react"
+import { useBudgets, UNCATEGORIZED_BUDGET_ID } from "../contexts/BudgetsContext"
+
+export default function AddExpenseModal({
+  show,
+  handleClose,
+  defaultBudgetId,
+}) {
+  const descriptionRef = useRef()
+  const amountRef = useRef()
+  const budgetIdRef = useRef()
+  const { addExpense, budgets } = useBudgets()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    addExpense({
+      description: descriptionRef.current.value,
+      amount: parseFloat(amountRef.current.value),
+      budgetId: budgetIdRef.current.value,
+    })
+    handleClose()
+  }
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>Nouveau Dépense</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control ref={descriptionRef} type="text" required />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="amount">
+            <Form.Label>Montant Dépense</Form.Label>
+            <Form.Control
+              ref={amountRef}
+              type="number"
+              required
+              min={0}
+              step={0.01}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="budgetId">
+            <Form.Label>Choix Budget</Form.Label>
+            <Form.Select defaultValue={defaultBudgetId} ref={budgetIdRef}>
+              <option id={UNCATEGORIZED_BUDGET_ID}>Hors Catégorie</option>
+              {budgets.map(budget => (
+                <option key={budget.id} value={budget.id}>
+                  {budget.name}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+          <div className="d-flex justify-content-end">
+            <Button variant="primary" type="submit">
+              Ajouter Dépense
+            </Button>
+          </div>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  )
+}
